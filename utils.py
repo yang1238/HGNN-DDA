84dbc8fc3e23e874254cd7af68c0816ef700bda0
yang1238 <151618173+yang1238@users.noreply.github.com>
2025-11-04 15:41:36 +0800

    Refactor multiomics_data function and labels
    Updated label output and variable names for clarity.

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 def multiomics_data():
 
-    #protein_fea
+    #disease_fea
     PHIN = np.genfromtxt("./2feature/disease_hin2vec.csv", delimiter=',', skip_header=1, dtype=np.dtype(str))
     ESM = np.genfromtxt("./2feature/disease_Word2Vec.csv", delimiter=',', skip_header=1, dtype=np.dtype(str))
     PHIN = np.array(PHIN)
@@ -26,50 +26,51 @@ def multiomics_data():
 
     ddi_adj = get_adj_array("./0data/drugdrug.txt")
 
-    # 加载label
+
     labellist = []
     with open('./0data/label.txt', 'r') as file:
         lines = file.readlines()
     for line in lines:
-        line = line.strip()  # 移除行首和行尾的空白字符
-        elements = line.split(" ")  # 使用空格分隔元素
-        processed_elements = [int(elements[1]), int(elements[0]), int(elements[2])]  # 互换位置
+        line = line.strip() 
+        elements = line.split(" ") 
+        processed_elements = [int(elements[1]), int(elements[0]), int(elements[2])] 
         labellist.append(processed_elements)
     labellist = torch.Tensor(labellist)
-    print("drug protein lable:", labellist.shape)
+    print("drug disease lable:", labellist.shape)
 
 
-    protein_HIN, protein_ESM, protein_adj = torch.FloatTensor(PHIN), torch.FloatTensor(ESM), torch.FloatTensor(ppi_adj)
+    disease_HIN, disease_ESM, disease_adj = torch.FloatTensor(PHIN), torch.FloatTensor(ESM), torch.FloatTensor(ppi_adj)
     drug_HIN, drug_PC, drug_adj = torch.FloatTensor(DHIN), torch.FloatTensor(PC), torch.FloatTensor(ddi_adj)
-    return protein_HIN, protein_ESM, protein_adj, drug_HIN, drug_PC, drug_adj, labellist
+    return disease_HIN, disease_ESM, disease_adj, drug_HIN, drug_PC, drug_adj, labellist
 
 
 def get_adj_array(file_path):
-    # 读取txt文件
-    file_path = file_path  # 替换为你的文件路径
+
+    file_path = file_path 
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
-    # 提取邻接矩阵信息
+
     adj_matrix = []
     for line in lines:
         row = [float(x) for x in line.strip().split()]  # 假设邻接矩阵中的元素以空格分隔
         adj_matrix.append(row)
 
-    # 将邻接矩阵转换为NumPy数组
+
     adj_array = np.array(adj_matrix)
     return adj_array
 
 
 def weight_variable_glorot(input_dim, output_dim, name=""):
-    #计算初始化范围 init_range，以保证权重在合理区间内。
+
     init_range = np.sqrt(6.0/(input_dim + output_dim))
-    #使用均匀分布初始化权重矩阵，范围在 [-init_range, init_range] 之间。
+
     initial = tf.compat.v1.random_uniform(
         [input_dim, output_dim],
         minval=-init_range,
         maxval=init_range,
         dtype=tf.compat.v1.float32
     )
-    #返回一个 TensorFlow 变量，表示初始化后的权重矩阵。
+
     return tf.compat.v1.Variable(initial, name=name)
+
